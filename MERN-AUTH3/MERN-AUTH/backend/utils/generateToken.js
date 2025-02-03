e4ac50631e04c48011741186bb10a6adea5b7e51
nurdinifatini1 <nurdinifatini@graduate.utm.my>
2025-02-03 17:44:54 +0800

    Update generateToken.js
    change the secure to true and sameSite to none

--- MERN-AUTH3/MERN-AUTH/backend/utils/generateToken.js
+++ MERN-AUTH3/MERN-AUTH/backend/utils/generateToken.js
@@ -7,8 +7,9 @@ const generateToken = (res, userId) => {
   
     res.cookie('jwt', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-      sameSite: 'strict', // Prevent CSRF attacks
+      secure: true,
+      //process.env.NODE_ENV !== 'development', // Use secure cookies in production
+      sameSite: 'none', // now using none since backend n frontend on diff server //use strict if backend n frontend in the same server    //if 'strict' Prevent CSRF attacks
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
   };
