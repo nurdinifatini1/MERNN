857beb04b7663bf74014c01472c03089b8b22c36
nurdinifatini1 <nurdinifatini@graduate.utm.my>
2025-02-03 18:12:53 +0800

    Update viteconfig.js
    include the VITE API URL to connect the backend server

--- MERN-AUTH3/MERN-AUTH/frontend/vite.config.js
+++ MERN-AUTH3/MERN-AUTH/frontend/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  server: {
+    historyApiFallback: true, // This ensures that React Router works correctly
+  }
+});
+
+// https://vitejs.dev/config/
+/*export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
@@ -14,3 +21,4 @@ export default defineConfig({
     },
   },
 });
+*/
