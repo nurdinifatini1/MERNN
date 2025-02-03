c6c59f91f075e21f1a0991a6013219d743840a08
nurdinifatini1 <nurdinifatini@graduate.utm.my>
2025-02-03 17:45:22 +0800

    Update server.js

--- MERN-AUTH3/MERN-AUTH/backend/server.js
+++ MERN-AUTH3/MERN-AUTH/backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -9,9 +10,21 @@ import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js';
 
+
 connectDB();
 
 const app = express();
+
+// Allow requests from your deployed frontend domain
+const allowedOrigins = 'https://mernn-frontend.vercel.app';
+
+app.use(
+  cors({ //cross origin resource sharing 
+    origin: allowedOrigins, // ✅ Your frontend URL
+    credentials: true, // ✅ Allow cookies
+    methods: 'GET,POST,PUT,DELETE',
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));  //allow send form data
@@ -20,14 +33,16 @@ app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
-if (process.env.NODE_ENV === 'production') {
+/*if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
   
     app.get('*', (req, res) =>
       res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
     );
-  } else {
+  }  */
+
+    {
     app.get('/', (req, res) => {
       res.send('API is running....');
     });
