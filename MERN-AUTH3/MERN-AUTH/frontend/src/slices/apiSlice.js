f8f44618883c0285975f0064e3976601cdca6ea2
nurdinifatini1 <nurdinifatini@graduate.utm.my>
2025-02-03 18:46:07 +0800

    Update apislice.js
    change basequery direct to vite api

--- MERN-AUTH3/MERN-AUTH/frontend/src/slices/apiSlice.js
+++ MERN-AUTH3/MERN-AUTH/frontend/src/slices/apiSlice.js
@@ -1,6 +1,10 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+//const baseQuery = fetchBaseQuery({ baseUrl: '' });
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_API_URL, // Ensure this points to your backend
+  credentials: 'include',  // This allows cookies to be sent
+});
 
 export const apiSlice = createApi({
   baseQuery,
